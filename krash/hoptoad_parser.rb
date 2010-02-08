76f118b2f2637d7889b187dc4a4806b18ae02ba9
Michael Bumann <michael@railslove.com>
2010-02-08 01:36:27 +0100

    use nokogiri #text() instead of iterating through the nodeset

--- krash/hoptoad_parser.rb
+++ krash/hoptoad_parser.rb
@@ -7,15 +7,15 @@ module Krash
     
     def application
       @application_settings ||= begin
-        node = xml.xpath("/notice/api-key").first.content
-        YAML.load(node) rescue {:access_key => node}
+        node = xml.xpath("/notice/api-key").text
+        YAML.load(node) rescue {}
       end
     end
     
     def exception
       {
-        :class => xml.xpath("/notice/error/class").collect(&:content).join(","),
-        :message => xml.xpath("/notice/error/message").collect(&:content).join(","),
+        :class => xml.xpath("/notice/error/class").text,
+        :message => xml.xpath("/notice/error/message").text,
         :backtrace => xml.xpath("/notice/error/backtrace").to_s,
         :request => xml.xpath("/notice/request").to_s,
         :environemt => xml.xpath("/notice/server-environment").to_s
