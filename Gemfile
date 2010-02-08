3ce54b9946ff2d3f42f028447e72ffde8f28eb15
Michael Bumann <michael@railslove.com>
2010-02-08 20:21:58 +0100

    update bundler api

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
+# A sample Gemfile
+source :gemcutter
+#
+# gem "rails"
+gem 'builder', '2.1.2'
 gem 'sinatra', '0.9.4'
 gem 'nokogiri', '1.4.1'
 gem 'httparty', '0.5.2'
-gem 'reddavis-clickatell'
+gem 'reddavis-clickatell', :require => "clickatell"
 gem 'prowl', '0.1.3'
--- krash.rb
+++ krash.rb
@@ -1,5 +1,13 @@
-require File.dirname(__FILE__) + '/vendor/gems/environment'
-Bundler.require_env
+begin
+  # Require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+Bundler.require(:default)
 
 require "krash/configuration"
 require "krash/hoptoad_parser"
