919c7408d99dae5205f5e2a644909bb471635225
reddavis <reddavis@gmail.com>
2010-02-11 13:19:58 +0000

    add codebase specs

--- spec/notifiers/codebase_spec.rb
+++ spec/notifiers/codebase_spec.rb
@@ -5,31 +5,32 @@ describe Krash::Notifiers::Codebase do
   
   before do
     @default_config = Krash::Configuration.new do
-      api_keys "key"
-      only /.*/
+      api_key "key"
+      priority_id 9
+      status_id 3
+      user 'captain_hook'
     end
-    @notifier = Krash::Notifiers::Iphone.new(@default_config)
+    @notifier = Krash::Notifiers::Codebase.new(@default_config)
   end
   
-  #describe "Notification" do
-  #  it "should not notify if the exception matches does not match the pattern" do
-  #    Prowl.should_not_receive(:add)
-  #    @notifier.config.only = /\d/  # Set the pattern to match
-  #    @notifier.notify(:exception => {:message => "not number"} ).should == false
-  #  end
-  #  
-  #  it "should notify if the exception message matches a certain pattern" do
-  #    @notifier.config.only = /\d/
-  #    
-  #    Prowl.should_receive(:add).with(hash_including({
-  #                                                    :event => "Exception in #{exception[:application][:project]}",
-  #                                                    :application => 'Krash!',
-  #                                                    :apikey => @default_config.api_keys,
-  #                                                    :description => exception[:exception][:message]
-  #                                                  }))
-  #    
-  #    @notifier.notify(exception)
-  #  end
-  #end
+  describe "Notification" do
+    it "should create a ticket" do
+      codebase_project = mock(:codebase_project)
+      
+      CodebaseProject.should_receive(:new).with(hash_including(
+                                                                :user => @default_config.user,
+                                                                :api_key => @default_config.api_key,
+                                                                :project => exception[:application][:project]
+                                                              )).and_return(codebase_project)
+                                                              
+      codebase_project.should_receive(:create_ticket).with(hash_including(
+                                                                          :"priority-id" => @default_config.priority_id,
+                                                                          :"status-id" => @default_config.status_id,
+                                                                          :summary => exception[:exception][:message]
+                                                                          ))
+      
+      @notifier.notify(exception)
+    end
+  end
 
 end
